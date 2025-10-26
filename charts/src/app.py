6f5391753df430ef9584de1cc4e3ed7cfaf08803
Albert Chisoro <chisoroalbert@gmail.com>
2025-10-26 19:47:00 +0200

    Made some changes

--- charts/src/app.py
+++ charts/src/app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 @app.route('/details', methods=['GET'])
 def details():
     return jsonify({
-        "message": "My Flask Application"
+        "message": "My Flask Application hey"
     })
 
 @app.route('/health', methods=['GET'])
